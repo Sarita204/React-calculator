2d272834d54a8d29b0797475ea5f5e4abd93ff04
sarita <[user>
2023-09-10 22:27:10 +0530

    Calculator using react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calculater from './components/calculater';
+
+
+function App() {
+  return(
+    <div className="APP">
+      <Calculater/>
+
+    </div>
+  )
+}
+export default App;
